bf41387b54c01197251248cc5956531d7b119aee
JGK1117 <jgon519@gmail.com>
2025-03-06 17:58:03 +0900

    feat: jinja2 practice

--- apps/api/v1/l_fastapi.py
+++ apps/api/v1/l_fastapi.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import (
-    PlainTextResponse, JSONResponse
+    PlainTextResponse, JSONResponse, HTMLResponse
 )
+from fastapi.templating import Jinja2Templates
+import os
+
+TEMPLATES_DIR: str = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'templates')
+templates: Jinja2Templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 api_router_l_fastapi: APIRouter = APIRouter(
     prefix='/l_fastapi', 
@@ -19,5 +24,33 @@ async def test_plaintext():
     '/json',
     response_class=JSONResponse,
 )
-async def test_json():
+async def test_json():\
     return {'content': 'hello'}
+
+@api_router_l_fastapi.get(
+    '/html',
+    response_class=HTMLResponse,
+)
+async def test_html():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    # 동일한 방법
+    # return HTMLResponse(content=html_content, status_code=200)
+    return html_content
+
+@api_router_l_fastapi.get(
+    '/jinja2/{id}',
+    response_class=HTMLResponse,
+)
+async def test_jinja2(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name='item.html', context={"id": id}
+    )
--- apps/config.py
+++ apps/config.py
@@ -2,11 +2,14 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from .api.v1 import api_router
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 
+# 환경 변수 설정, 바인딩할 변수 명을 class 내에서 선언해줘야 함.
 class Setting(BaseSettings):
     model_config = SettingsConfigDict(env_file=BASE_DIR)
 
@@ -21,6 +24,9 @@ def create_app() -> FastAPI:
         allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
         allow_headers=["*"],  # 모든 헤더 허용
     )
+
+    # 정적 파일 저장소 마운트, css, js 등
+    app.mount('/static', StaticFiles(directory=STATIC_DIR), name='static')
 
     # 하위 엔드포인트 APIRouter 추가 
     app.include_router(api_router)
